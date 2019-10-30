30f1958c2cf4554d3a31561acbe1c9a1a95df434
trapintrovert <mcneo062@gmail.com>
2019-10-30 13:14:52 +0100

    updated shop data with new data, updated cart utils to group items in cart reducer

--- src/App.js
+++ src/App.js
@@ -68,7 +68,7 @@ const mapStateToProps = ({ user }) => ({
 })
 
 const mapDispatchToProps = dispatch => ({
-  setCurrentUser: user=> dispatch(setCurrentUser(user))
+  setCurrentUser: user => dispatch(setCurrentUser(user))
 })
 
 export default connect(
